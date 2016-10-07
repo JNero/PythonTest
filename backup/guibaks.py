9bf5ede96bd872b39535e61aca69207cd05863dc
Charles Joe <qkywy2009@163.com>
2016-10-07 17:15:20 +0100

    客户端2.0

--- backup/guibaks.py
+++ backup/guibaks.py
@@ -2,10 +2,88 @@ from tkinter import *
 from tkinter.ttk import *
 import socket
 import struct
+import os
+import pickle
+
+BAK_PATH=r'/home/qiao/桌面/bak'
+
+def recv_unit_data(clnt,infos_len):
+	data=b''
+	if 0<infos_len<=1024:
+		data+=clnt.recv(infos_len)
+	else:
+		while True:
+			if infos_len>1024:
+				data+=clnt.recv(1024)
+				infos_len-=1024
+			else:
+				data+=clnt.recv(infos_len)
+				break
+	return data
+
+def get_files_info(clnt):
+	fmt_str='Q'
+	headsize=struct.calcsize(fmt_str)
+	data=clnt.recv(headsize)
+	infos_len=struct.unpack(fmt_str,data)
+	data=recv_unit_data(clnt,infos_len)
+	return pickle.load(data)
+
+def mk_path():
+	paths=filepath.split(os.path.seq)[:-1]
+	p=BAK_PATH
+	for path in paths:
+		p=os.path.join(p,path)
+		if not os.path.exists(p):
+			os.mkdir
+
+def recv_file(clnt,infos_len,filepath):
+	mk_path(filepath)
+	filepath=os.path.join(BAK_PATH,filepath)
+	f=open(filepath,'wb+')
+	try:
+		if 0<infos_len<=1024:
+			data=clnt.recv(infos_len)
+			f.write(data)
+		else:
+			while True:
+				if infos_len>1024:
+					data=clnt.recv(1024)
+					f.write(data)
+					infos_len-=1024
+				else:
+					data =clnt.recv(1024)
+					f.write(data)
+					break
+	except:
+		peint('error')
+	else:
+		return True
+	finally:
+		f.close()
 
 
 def start(host,port):
-	pass
+	if not os.path.exists(BAK_PATH):
+		os.mkdir(BAK_PATH)
+	st=socket.socket()
+	st.bind((host,port))
+	st.listen(1)
+	client,addr=st.accept()
+	files_lst=get_files_info(client)
+	for size,filepath in files_lst:
+		res=recv_file(client,size,filepath)
+		send_echo(client,res)
+
+	client.close()
+	st.close()
+
+def send_echo(clnt,res):
+	if res:
+		clnt.sendall(b'success')
+	else:
+		clnt.sendall(b'failure')
+
 
 class MyFrame(Frame):
 	"""docstring for ClassName"""
@@ -51,7 +129,7 @@ class MyFrame(Frame):
 
 	def start_server(self):
 		print(self.server_ip.get(),self.server_port.get())
-		start(self.server_ip.get(),self.server_port.get())
+		start(self.server_ip.get(),int(self.server_port.get()))
 
 if __name__ == '__main__':
 	root=Tk()
